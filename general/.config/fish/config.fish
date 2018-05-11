e6d52c60598fa14d818353a6826536fe5f935d52
errr <r.t@u-8.lu>
2018-05-11 04:11:18 +0300

    Fish workaround for emoji (https://github.com/fish-shell/fish-shell/issues/2652)

--- general/.config/fish/config.fish
+++ general/.config/fish/config.fish
@@ -2,3 +2,4 @@ set -g __fish_git_prompt_showdirtystate 1
 set -g __fish_git_prompt_showstashstate 1
 set -g __fish_git_prompt_showuntrackedfiles 1
 set -g __fish_git_prompt_showupstream auto
+set -g fish_emoji_width 2
